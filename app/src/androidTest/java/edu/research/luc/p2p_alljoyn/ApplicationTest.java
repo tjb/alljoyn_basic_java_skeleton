ff076dceb964530e036a6014f98253dc70cd7ab0
tylerbobella <tjbobella@gmail.com>
2015-11-20 18:12:34 -0600

    init

---
+++ app/src/androidTest/java/edu/research/luc/p2p_alljoyn/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.research.luc.p2p_alljoyn;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
