6998061b26e38d8fa3ad28e1fd139aedb8edd126
Griffin Moe <me@griffinmoe.com>
2015-11-28 13:48:50 -0600

    Limes.

--- app/src/main/java/edu/research/luc/p2p_alljoyn/MainActivity.java
+++ app/src/main/java/edu/research/luc/p2p_alljoyn/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("alljoyn_java", "Can not hold all limes.");
     }
 
     @Override
